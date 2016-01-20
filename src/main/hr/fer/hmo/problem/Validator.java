b13d6823875008d4dd8f4f5d0f06c93580c3f2df
dstank25 <domagoj.stankovic@gmail.com>
2016-01-20 14:22:58 +0100

    Validator - validate hard constraints split into batch and per employee validation

--- src/main/hr/fer/hmo/problem/Validator.java
+++ src/main/hr/fer/hmo/problem/Validator.java
@@ -62,6 +62,8 @@ public class Validator {
   }
 
 
+  // SOFT CONSTRAINTS
+
   private int validateShiftCovers(Instance instance, Solution solution) {
     List<ShiftCover> shiftCovers = instance.getShiftCovers();
     int totalWeight = 0;
@@ -122,17 +124,27 @@ public class Validator {
   }
 
 
+  // HARD CONSTRAINTS
+
   private boolean validateDaysOff(Instance instance, Solution solution) {
-    Map<String, DaysOff> employeeDaysOff = instance.getEmployeeDaysOff();
     for (String employeeId : solution.getEmployeeIds()) {
       // for each employee
-      List<Integer> daysOff = employeeDaysOff.get(employeeId).getDaysOff();
-      for (Integer dayOff : daysOff) {
-        String shift = solution.getShift(employeeId, dayOff);
-        if (shift != null) {
-          // employee is working on a day off
-          return false;
-        }
+      if (!validateDaysOff(instance, solution, employeeId)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+  public boolean validateDaysOff(Instance instance, Solution solution, String employeeId) {
+    Map<String, DaysOff> employeeDaysOff = instance.getEmployeeDaysOff();
+    List<Integer> daysOff = employeeDaysOff.get(employeeId).getDaysOff();
+    for (Integer dayOff : daysOff) {
+      String shift = solution.getShift(employeeId, dayOff);
+      if (shift != null) {
+        // employee is working on a day off
+        return false;
       }
     }
     return true;
@@ -140,23 +152,9 @@ public class Validator {
 
 
   private boolean validateMaxWeekends(Instance instance, Solution solution) {
-    int days = solution.getHorizon();
-    Map<String, Employee> employees = instance.getEmployees();
     for (String employeeId : solution.getEmployeeIds()) {
       // for each employee
-      Employee employee = employees.get(employeeId);
-      int maxWeekends = employee.getMaxWeekends();
-      int workingWeekends = 0;
-      for (int i = 5; i < days; i+=7) {
-        // for each saturday
-        String saturdayShift = solution.getShift(employeeId, i);
-        String sundayShift = solution.getShift(employeeId, i+1);
-        if (saturdayShift != null || sundayShift != null) {
-          // working weekend
-          workingWeekends++;
-        }
-      }
-      if (workingWeekends > maxWeekends) {
+      if (!validateMaxWeekends(instance, solution, employeeId)) {
         return false;
       }
     }
@@ -164,34 +162,64 @@ public class Validator {
   }
 
 
-  private boolean validateConsecutiveDaysOff(Instance instance, Solution solution) {
+  public boolean validateMaxWeekends(Instance instance, Solution solution, String employeeId) {
     int days = solution.getHorizon();
     Map<String, Employee> employees = instance.getEmployees();
+    Employee employee = employees.get(employeeId);
+    int maxWeekends = employee.getMaxWeekends();
+    int workingWeekends = 0;
+    for (int i = 5; i < days; i+=7) {
+      // for each saturday
+      String saturdayShift = solution.getShift(employeeId, i);
+      String sundayShift = solution.getShift(employeeId, i+1);
+      if (saturdayShift != null || sundayShift != null) {
+        // working weekend
+        workingWeekends++;
+      }
+    }
+    if (workingWeekends > maxWeekends) {
+      return false;
+    }
+    return true;
+  }
+
+
+  private boolean validateConsecutiveDaysOff(Instance instance, Solution solution) {
     for (String employeeId : solution.getEmployeeIds()) {
       // for each employee
-      Employee employee = employees.get(employeeId);
-      int minConsecutiveDaysOff = employee.getMinConsecutiveDaysOff();
-      int streak = 0;
-      int start = 0;
-      while (solution.getShift(employeeId, start) == null) {
-        start++;
+      if (!validateConsecutiveDaysOff(instance, solution, employeeId)) {
+        return false;
       }
-      for (int i = start; i < days; i++) {
-        // for each day
-        String shift = solution.getShift(employeeId, i);
-        if (shift != null) {
-          // employee works on this day
-          if (streak > 0) {
-            // end of streak
-            if (streak < minConsecutiveDaysOff) {
-              return false;
-            }
-            streak = 0;
+    }
+    return true;
+  }
+
+
+  public boolean validateConsecutiveDaysOff(Instance instance, Solution solution, String employeeId) {
+    int days = solution.getHorizon();
+    Map<String, Employee> employees = instance.getEmployees();
+    Employee employee = employees.get(employeeId);
+    int minConsecutiveDaysOff = employee.getMinConsecutiveDaysOff();
+    int streak = 0;
+    int start = 0;
+    while (solution.getShift(employeeId, start) == null) {
+      start++;
+    }
+    for (int i = start; i < days; i++) {
+      // for each day
+      String shift = solution.getShift(employeeId, i);
+      if (shift != null) {
+        // employee works on this day
+        if (streak > 0) {
+          // end of streak
+          if (streak < minConsecutiveDaysOff) {
+            return false;
           }
-        } else {
-          // employee does not work on this day
-          streak++;
+          streak = 0;
         }
+      } else {
+        // employee does not work on this day
+        streak++;
       }
     }
     return true;
@@ -199,60 +227,51 @@ public class Validator {
 
 
   private boolean validateConsecutiveShifts(Instance instance, Solution solution) {
-    int days = solution.getHorizon();
-    Map<String, Employee> employees = instance.getEmployees();
     for (String employeeId : solution.getEmployeeIds()) {
       // for each employee
-      Employee employee = employees.get(employeeId);
-      int minConsecutiveShifts = employee.getMinConsecutiveShifts();
-      int maxConsecutiveShifts = employee.getMaxConsecutiveShifts();
-      int streak = 0;
-      for (int i = 0; i < days; i++) {
-        // for each day
-        String shift = solution.getShift(employeeId, i);
-        if (shift == null) {
-          // employee does not work on this day
-          if (streak > 0) {
-            // end of streak
-            if (streak < minConsecutiveShifts || streak > maxConsecutiveShifts) {
-              return false;
-            }
-            streak = 0;
-          }
-        } else {
-          // employee works on this day
-          streak++;
-        }
-      }
-      if (streak < minConsecutiveShifts || streak > maxConsecutiveShifts) {
+      if (!validateConsecutiveShifts(instance, solution, employeeId)) {
         return false;
       }
+    }
+    return true;
+  }
+
+
+  public boolean validateConsecutiveShifts(Instance instance, Solution solution, String employeeId) {
+    int days = solution.getHorizon();
+    Map<String, Employee> employees = instance.getEmployees();
+    Employee employee = employees.get(employeeId);
+    int minConsecutiveShifts = employee.getMinConsecutiveShifts();
+    int maxConsecutiveShifts = employee.getMaxConsecutiveShifts();
+    int streak = 0;
+    for (int i = 0; i < days; i++) {
+      // for each day
+      String shift = solution.getShift(employeeId, i);
+      if (shift == null) {
+        // employee does not work on this day
+        if (streak > 0) {
+          // end of streak
+          if (streak < minConsecutiveShifts || streak > maxConsecutiveShifts) {
+            return false;
+          }
+          streak = 0;
+        }
+      } else {
+        // employee works on this day
+        streak++;
+      }
+    }
+    if (streak < minConsecutiveShifts || streak > maxConsecutiveShifts) {
+      return false;
     }
     return true;
   }
 
 
   private boolean validateTotalMinutes(Instance instance, Solution solution) {
-    int days = solution.getHorizon();
-    Map<String, Employee> employees = instance.getEmployees();
-    Map<String, Shift> shifts = instance.getShifts();
     for (String employeeId : solution.getEmployeeIds()) {
       // for each employee
-      int minutesCount = 0;
-      for (int i = 0; i < days; i++) {
-        // for each day
-        String shiftId = solution.getShift(employeeId, i);
-
-        // Do not count if day off
-        if (shiftId == null) {
-          continue;
-        }
-
-        int length = shifts.get(shiftId).getLength();
-        minutesCount += length;
-      }
-      Employee employee = employees.get(employeeId);
-      if (minutesCount < employee.getMinTotalMinutes() || minutesCount > employee.getMaxTotalMinutes()) {
+      if (!validateTotalMinutes(instance, solution, employeeId)) {
         return false;
       }
     }
@@ -260,32 +279,65 @@ public class Validator {
   }
 
 
-  private boolean validateMaxShifts(Instance instance, Solution solution) {
+  public boolean validateTotalMinutes(Instance instance, Solution solution, String employeeId) {
     int days = solution.getHorizon();
     Map<String, Employee> employees = instance.getEmployees();
+    Map<String, Shift> shifts = instance.getShifts();
+    int minutesCount = 0;
+    for (int i = 0; i < days; i++) {
+      // for each day
+      String shiftId = solution.getShift(employeeId, i);
+
+      // Do not count if day off
+      if (shiftId == null) {
+        continue;
+      }
+
+      int length = shifts.get(shiftId).getLength();
+      minutesCount += length;
+    }
+    Employee employee = employees.get(employeeId);
+    if (minutesCount < employee.getMinTotalMinutes() || minutesCount > employee.getMaxTotalMinutes()) {
+      return false;
+    }
+    return true;
+  }
+
+
+  private boolean validateMaxShifts(Instance instance, Solution solution) {
     for (String employeeId : solution.getEmployeeIds()) {
       // for each employee
-      Map<String, Integer> shiftsCount = new HashMap<>(); // shift -> shiftsCount
-      for (int i = 0; i < days; i++) {
-        // for each day
-        String shiftId = solution.getShift(employeeId, i);
-
-        // Do not count days off
-        if (shiftId == null) {
-          continue;
-        }
-
-        if (!shiftsCount.containsKey(shiftId)) {
-          shiftsCount.put(shiftId, 0);
-        }
-        shiftsCount.put(shiftId, shiftsCount.get(shiftId) + 1);
+      if (!validateMaxShifts(instance, solution, employeeId)) {
+        return false;
       }
-      Employee employee = employees.get(employeeId);
-      Map<String, Integer> maxShifts = employee.getMaxShifts();
-      for (String shift : shiftsCount.keySet()) {
-        if (shiftsCount.get(shift) > maxShifts.get(shift)) {
-          return false;
-        }
+    }
+    return true;
+  }
+
+
+  public boolean validateMaxShifts(Instance instance, Solution solution, String employeeId) {
+    int days = solution.getHorizon();
+    Map<String, Employee> employees = instance.getEmployees();
+    Map<String, Integer> shiftsCount = new HashMap<>(); // shift -> shiftsCount
+    for (int i = 0; i < days; i++) {
+      // for each day
+      String shiftId = solution.getShift(employeeId, i);
+
+      // Do not count days off
+      if (shiftId == null) {
+        continue;
+      }
+
+      if (!shiftsCount.containsKey(shiftId)) {
+        shiftsCount.put(shiftId, 0);
+      }
+      shiftsCount.put(shiftId, shiftsCount.get(shiftId) + 1);
+    }
+    Employee employee = employees.get(employeeId);
+    Map<String, Integer> maxShifts = employee.getMaxShifts();
+    for (String shift : shiftsCount.keySet()) {
+      if (shiftsCount.get(shift) > maxShifts.get(shift)) {
+        return false;
       }
     }
     return true;
@@ -293,17 +345,24 @@ public class Validator {
 
 
   private boolean validateShiftRotation(Instance instance, Solution solution) {
-    Map<String, Shift> shifts = instance.getShifts();
-    int days = solution.getHorizon();
     for (String employeeId : solution.getEmployeeIds()) {
       // for each employee
-      for (int i = 0; i < days - 1; i++) {
-        // for each day except last one
-        String currShift = solution.getShift(employeeId, i);
-        String nextShift = solution.getShift(employeeId, i+1);
-        if (shifts.get(currShift).getNotFollowingShifts().contains(nextShift)) {
-          return false;
-        }
+      if (!validateShiftRotation(instance, solution, employeeId)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public boolean validateShiftRotation(Instance instance, Solution solution, String employeeId) {
+    Map<String, Shift> shifts = instance.getShifts();
+    int days = solution.getHorizon();
+    for (int i = 0; i < days - 1; i++) {
+      // for each day except last one
+      String currShift = solution.getShift(employeeId, i);
+      String nextShift = solution.getShift(employeeId, i+1);
+      if (shifts.get(currShift).getNotFollowingShifts().contains(nextShift)) {
+        return false;
       }
     }
     return true;
